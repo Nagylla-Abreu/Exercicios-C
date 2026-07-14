6dfba513ff5e29e4c5ed79a60d8a6e24cf191a99
Nagylla-Abreu <nagyllaabreu08@gmail.com>
2026-07-13 22:35:40 -0300

    exercícios

---
+++ cap06/matrizes/ex06.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+
+int main(){
+
+    int matriz[3][3];
+    int i, j, soma=0;
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("Insira o valor da posicao [%d][%d]: ", i, j);
+            scanf("%d", &matriz[i][j]);
+        }
+    }
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            printf("%d ", matriz[i][j]);
+        }
+        printf("\n");
+    }
+
+    for(i=0;i<3;i++){
+        for(j=0;j<3;j++){
+            if(i==j){
+                soma = soma + matriz[i][j];
+            }
+        }
+    }
+
+    printf("A soma dos valores da diagonal principal eh: %d\n", soma);
+
+    return 0;
+}
